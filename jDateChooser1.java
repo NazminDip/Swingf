ae22d4befb3d7cfba282ef86825077e24e0c89e6
NazminDip <48346432+NazminDip@users.noreply.github.com>
2019-07-08 19:47:58 +0600

    Add files via upload

---
+++ jDateChooser1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Com.pro;
+
+/**
+ *
+ * @author Dipty
+ */
+class jDateChooser1 {
+
+    static Object getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
